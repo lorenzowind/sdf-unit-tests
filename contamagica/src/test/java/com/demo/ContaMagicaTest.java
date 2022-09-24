3a7098dd4d9040d83772796de15e75ee16548519
Niitto <103511960+Niitto@users.noreply.github.com>
2022-09-24 15:51:38 -0300

    alterei/comentei testes

--- contamagica/src/test/java/com/demo/ContaMagicaTest.java
+++ contamagica/src/test/java/com/demo/ContaMagicaTest.java
@@ -11,43 +11,47 @@ import org.junit.jupiter.api.Test;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ContaMagicaTest {
-    private ContaMagica cm = null;
+    private ContaMagica cm;
 
-    @BeforeAll
-    void setUp() {
-        cm = new ContaMagica("1234-100123", "Bob Esponja");
+    @BeforeEach
+    public void setUp() {
+       cm = new ContaMagica("12010-12010", "Bob Esponja");
     }
     
-    @AfterEach
-    void cleanAcc(){
-        cm.retirada(cm.getSaldo());
-    }
 
     // MOVIMENTAÇÔES BÁSICAS DE CONTA DE BANCO
     @Test
     public void retiradaDeValorAleatorioMaiorQueSaldoRetornaFalse() {
-        cm.deposito(29000.00);
-        double doubleDeposito = cm.getSaldo() * 2;
-        Assertions.assertEquals(false, ThreadLocalRandom.current().nextDouble(cm.getSaldo(), doubleDeposito + 1));
+        cm.deposito(24000.00);
+        double maxValorParaRetirada = cm.getSaldo() * 2;
+        Assertions.assertEquals(false, ThreadLocalRandom.current().nextDouble(cm.getSaldo(), maxValorParaRetirada + 1));
     }
+
     @Test
     public void retiradaDeValorAleatorioMenorIgualQueSaldoRetornaTrue() {
-        cm.deposito(29000.00);
+        cm.deposito(24000.00);
         Assertions.assertEquals(true, cm.retirada(ThreadLocalRandom.current().nextDouble(1, cm.getSaldo() + 1)));
     }
 
     // CRIAÇÃO INAPROPRIADA DE CONTAS
+    // @Test
+    // public void criaContaComNumeroErrado() {
+    //     try {
+    //         ContaMagica CONTA_TESTE = new ContaMagica("0000000", "Nicolas0192");    
+    //         Assertions.assertEquals(true, false);
+    //     } catch (IllegalNumberException e) {
+    //     }
+    // }
+
+    // UPGRADE/DOWNGRADE DE CATEGORIA DA CONTA
     @Test
-    public void criaContaComNumeroErrado() {
-        try {
-            ContaMagica CONTA_TESTE = new ContaMagica("0000000", "Nicolas0192");    
-            Assertions.assertEquals(true, false);
-        } catch (IllegalNumberException e) {
-        }
+    public void depositaDinheiroMasFazUpgradeParaMaisDeUmaCategoria(){
+        cm.deposito(200000.00);
+        Assertions.assertEquals(Categoria.GOLD, cm.getCategoria());
     }
 
-    @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
-    }
+    // @Test
+    // public void shouldAnswerWithTrue() {
+    //     assertTrue(true);
+    // }
 }
